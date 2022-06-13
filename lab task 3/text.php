4aa29116ed8701e439dda348a235231273f39480
tasnubatasnim <106444050+tasnubatasnim@users.noreply.github.com>
2022-06-13 21:51:55 +0600

    Add files via upload
    lab task 3(php data handing)

---
+++ lab task 3/text.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<h1>User login</h1>
+<form action="welcome.php" method="post">
+User name: <input type="text" name="name"><br>
+password: <input type="password" name="passsword"><br>
+<hr>
+<input type="checkbox" id="RM" name="RM" value="Remember Me">
+  <label for="Remember Me">Remember me</label><br>
+<input type="submit">
+<a href="">forget possword<a>
+
+</form>
+
+
+</body>
+</html>
---
+++ lab task 3/text1.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<h1>Changed password</h1>
+<form action="welcome.php" method="post">
+Currentpassword: <input type="password" name="passsword"><br>
+New password: <input type="password" name="passsword"><br>
+Retype New Password: <input type="password" name="passsword"><br>
+<hr>
+
+<input type="submit">
+
+</form>
+
+
+</body>
+</html>
